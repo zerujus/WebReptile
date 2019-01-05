5c8330455da1f6da8d52ac9779278b4a43707558
Ginger <18390938127@163.com>
2019-01-05 17:33:33 +0800

    add search ebook by recommend

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 # -*- coding : utf-8 -*-
-import urllib.request
 import re
 import os
+import urllib
 from concurrent.futures.thread import ThreadPoolExecutor
 from urllib.parse import quote
 
+import requests
 from bs4 import BeautifulSoup
 
 
@@ -14,15 +15,15 @@ class Ebook():
     def __int__(self, type, thread):
         self.info = 'Ebook'
         self.type = type
-        self.book_list = []
         self.fileDir = 'D:\\EBook'
-        self.max_thread = thread
+        self.pool = ThreadPoolExecutor(max_workers=thread)
 
     '''try url to web page'''
 
     def get_html(self, url):
-        page = urllib.request.urlopen(url)
-        return page.read().decode('utf-8', errors='replace')
+        html = requests.get(url)
+        html.encoding = 'utf-8'
+        return html.text
 
     '''write data in file'''
 
@@ -31,28 +32,10 @@ class Ebook():
         pageFile.write(booklist)
         pageFile.close()
 
-    '''find book list in web page'''
-
-    def analysis_booklist(self, htmlcode):
-        print('start analysis_booklist')
-        soup = BeautifulSoup(htmlcode, 'html.parser')
-        book_target = soup.find_all('a', target='_blank')
-        html_book_list = []
-        for item in book_target:
-            book_item = BeautifulSoup(str(item), 'html.parser')
-            book_href = book_item.find('a').get('href')
-            result = re.match('https://www.80txt.com(/\w+)+.html', book_href)
-            if result:
-                html_book_list.append(book_href)
-        if html_book_list.__sizeof__() == 0:
-            return None
-        else:
-            return html_book_list
-
     '''find download page in book item page'''
 
     def analysis_book(self, htmlcode):
-        print('start analysis_book')
+        print('Book : ' + htmlcode)
         soup = BeautifulSoup(self.get_html(htmlcode), 'html.parser')
         module = soup.find_all('a', id='read_book')
         for item in module:
@@ -98,7 +81,6 @@ class Ebook():
             print('已存在:' + fileName.split('.')[0])
 
     def thread_bookList(self, book):
-        print(str(book))
         book_down = self.analysis_book(book)
         if book_down:
             book_path = self.analysis_down(book_down)
@@ -108,14 +90,24 @@ class Ebook():
     '''search ebook by annunciation'''
     def bookList_annunciation(self, htmlcode):
         print('bookList_annunciation')
-        pool = ThreadPoolExecutor(max_workers=self.max_thread)
         soup = BeautifulSoup(self.get_html(htmlcode), 'html.parser')
         slist = soup.find('div', id='slist')
         if slist is not None:
             bookList_introduction = slist.find_all('a', target='_blank')
             for book_introduction in bookList_introduction:
-                pool.submit(self.thread_bookList, book_introduction.get('href'))
+                self.pool.submit(self.thread_bookList, book_introduction.get('href'))
 
+    '''search ebook by recommend'''
+    def bookList_recommend(self, htmlcode):
+        print('bookList_recommend')
+        soup = BeautifulSoup(self.get_html(htmlcode), 'html.parser')
+        div = soup.find('div', id='tuijian')
+        if div is not None:
+            bookList = div.find_all('a')
+            for a in bookList:
+                result = re.match('https://\w+.80txt.com(/\w+)+.html', a.get('href'))
+                if result:
+                    self.pool.submit(self.thread_bookList, a.get('href'))
 
 '''eBook = Ebook()
 eBook.__int__(0)
@@ -129,4 +121,7 @@ for i in range(0, 10):
 
 Ebook = Ebook()
 Ebook.__int__(0, 7)
-Ebook.bookList_annunciation('https://www.80txt.com/sort/23.html')
+Ebook.bookList_recommend('https://www.80txt.com/')
+'''soup = BeautifulSoup(Ebook.get_html('https://www.80txt.com/txtxz/556.html'), 'html.parser')
+module = soup.find_all('a', id='read_book')
+print(str(module))'''
